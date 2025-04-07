921371f4fb7f44b2ea33b6140a9b046037307de8
Emre Özer <eozer1371@gmail.com>
2025-04-07 13:03:25 +0300

    Api yapıldı

---
+++ Learnigo.API/Mapping/AboutMapping.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Learnigo.DTO.Dtos.AboutDtos;
+using Learnigo.Entity.Entities;
+
+namespace Learnigo.API.Mapping
+{
+    public class AboutMapping:Profile
+    {
+        public AboutMapping()
+        {
+            CreateMap<CreateAboutDto, About>().ReverseMap();
+            CreateMap<UpdateAboutDto, About>().ReverseMap();
+        }
+    }
+}
--- Learnigo.API/Program.cs
+++ Learnigo.API/Program.cs
@@ -1,16 +1,22 @@
+using Learnigo.Business.Abstract;
+using Learnigo.Business.Concrete;
+using Learnigo.DataAccess.Abstract;
 using Learnigo.DataAccess.Context;
+using Learnigo.DataAccess.Repositories;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
 builder.Services.AddDbContext<LearnigoContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSqlConnection")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped(typeof(IGenericService<>), typeof(GenericManager<>));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
