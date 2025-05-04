aeec863dc082cf97947ff02391bf551ffb603626
Emre Özer <eozer1371@gmail.com>
2025-05-05 01:43:36 +0300

    Admin layout has been created and customized.

---
+++ Learnigo.WebUI/Areas/Admin/Controllers/AboutController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learnigo.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- Learnigo.WebUI/Program.cs
+++ Learnigo.WebUI/Program.cs
@@ -20,8 +20,19 @@ app.UseRouting();
 
 app.UseAuthorization();
 
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+      name: "areas",
+      pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+    );
+});
+
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+
+
 
 app.Run();
