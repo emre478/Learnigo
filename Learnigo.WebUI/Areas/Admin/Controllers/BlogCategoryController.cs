2f1b5ce7ed73ae734af321b303832d2aa39a7598
Emre Özer <eozer1371@gmail.com>
2025-05-10 16:33:56 +0300

    Blog Category Controller has been copleted.

---
+++ Learnigo.WebUI/Areas/Admin/Controllers/BlogCategoryController.cs
@@ -0,0 +1,52 @@
+﻿using Learnigo.WebUI.Dtos.BlogCategoryDtos;
+using Learnigo.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnigo.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class BlogCategoryController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+
+            var values = await _client.GetFromJsonAsync<List<ResultBlogCategoryDto>>("blogcategories");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteBlogCategory(int id)
+        {
+            await _client.DeleteAsync($"blogcatagories/{id}");
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        public IActionResult CreateBlogCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateBlogCategory(CreateBlogCategoryDto createBlogCategoryDto)
+        {
+            await _client.PostAsJsonAsync("blogcategories", createBlogCategoryDto);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> UpdateBlogCategory(int id)
+        {
+            var values = await _client.GetFromJsonAsync<UpdateBlogCategoryDto>($"blogcategories/{id}");
+            return View(values);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> UpdateBlogCategory(UpdateBlogCategoryDto updateBlogCategoryDto)
+        {
+            await _client.PutAsJsonAsync("blogcategories", updateBlogCategoryDto);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
