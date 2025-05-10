89726efaac712ac124b8b24acacd4ee26f89ce39
Emre Özer <eozer1371@gmail.com>
2025-05-10 19:30:49 +0300

    Admin's Blog List has been completed.

---
+++ Learnigo.DataAccess/Abstract/IBlogRepository.cs
@@ -0,0 +1,14 @@
+﻿using Learnigo.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnigo.DataAccess.Abstract
+{
+    public interface IBlogRepository : IRepository<Blog>
+    {
+        List<Blog> GetBlogsWithCategories();
+    }
+}
---
+++ Learnigo.DataAccess/Concrete/BlogRepository.cs
@@ -0,0 +1,27 @@
+﻿using Learnigo.DataAccess.Abstract;
+using Learnigo.DataAccess.Context;
+using Learnigo.DataAccess.Repositories;
+using Learnigo.Entity.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnigo.DataAccess.Concrete
+{
+    public class BlogRepository : GenericRepository<Blog>, IBlogRepository
+    {
+        private readonly LearnigoContext _lercontext;
+        public BlogRepository(LearnigoContext _context) : base(_context)
+        {
+            _lercontext = _context;
+        }
+
+        public List<Blog> GetBlogsWithCategories()
+        {
+            return _lercontext.Blogs.Include(x => x.BlogCategory).ToList();
+        }
+    }
+}
