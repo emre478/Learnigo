49808a6917a5bd737c514861cb667b6ae0d9f0d2
Emre Özer <eozer1371@gmail.com>
2025-05-12 21:57:49 +0300

    Admin's Contact Controller was made.

---
+++ Learnigo.WebUI/Areas/Admin/Controllers/ContactController.cs
@@ -0,0 +1,54 @@
+﻿using Learnigo.WebUI.Dtos.ContactDtos;
+using Learnigo.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnigo.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("[area]/[controller]/[action]/{id?}")]
+    public class ContactController : Controller
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IActionResult> Index()
+        {
+
+            var values = await _client.GetFromJsonAsync<List<ResultContactDto>>("Contacts");
+            return View(values);
+        }
+
+        public async Task<IActionResult> DeleteContact(int id)
+        {
+            await _client.DeleteAsync("Contacts/" + id);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public IActionResult CreateContact()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateContact(CreateContactDto createContactDto)
+        {
+            await _client.PostAsJsonAsync("Contacts", createContactDto);
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateContact(int id)
+        {
+           
+            var values = await _client.GetFromJsonAsync<UpdateContactDto>("Contacts/" + id);
+            return View(values);
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> UpdateContact(UpdateContactDto updateContactDto)
+        {
+            await _client.PutAsJsonAsync("Contacts", updateContactDto);
+            return RedirectToAction("Index");
+        }
+    }
+}
