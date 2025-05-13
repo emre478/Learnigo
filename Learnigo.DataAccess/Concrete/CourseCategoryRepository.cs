6a615e0086ec8d04b3f535a91046834999d0468e
Emre Özer <eozer1371@gmail.com>
2025-05-14 00:02:00 +0300

    Course Category Concrete has been completed.

---
+++ Learnigo.DataAccess/Concrete/CourseCategoryRepository.cs
@@ -0,0 +1,34 @@
+﻿using Learnigo.DataAccess.Abstract;
+using Learnigo.DataAccess.Context;
+using Learnigo.DataAccess.Repositories;
+using Learnigo.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnigo.DataAccess.Concrete
+{
+    public class CourseCategoryRepository : GenericRepository<CourseCategory>, ICourseCategoryRepository
+    {
+        public CourseCategoryRepository(LearnigoContext _context) : base(_context)
+        {
+            
+        }
+
+        public void DontShowOnHome(int id)
+        {
+            var value = _context.Courses.Find(id);
+            value.IsShow = false;
+            _context.SaveChanges();
+        }   
+
+        public void ShowOnHome(int id)
+        {
+            var value = _context.Courses.Find(id);
+            value.IsShow = true;
+            _context.SaveChanges();
+        }
+    }
+}
