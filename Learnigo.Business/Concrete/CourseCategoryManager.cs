077ac385369289d38d18a5694fb4a1773655c1aa
Emre Özer <eozer1371@gmail.com>
2025-05-14 00:12:04 +0300

    CourseCategory Manager has been completed.

---
+++ Learnigo.Business/Concrete/CourseCategoryManager.cs
@@ -0,0 +1,31 @@
+﻿using Learnigo.Business.Abstract;
+using Learnigo.DataAccess.Abstract;
+using Learnigo.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnigo.Business.Concrete
+{
+    public class CourseCategoryManager : GenericManager<CourseCategory>, ICourseCategoryService
+    {
+        private readonly ICourseCategoryRepository _courseCategoryRepository;
+        public CourseCategoryManager(IRepository<CourseCategory> _repository, ICourseCategoryRepository courseCategoryRepository ) : base
+            (_repository)
+        {
+            _courseCategoryRepository = courseCategoryRepository;
+        }
+
+        public void TDontShowOnHome(int id)
+        {
+            _courseCategoryRepository.DontShowOnHome(id);
+        }
+
+        public void TShowOnHome(int id)
+        {
+            _courseCategoryRepository.ShowOnHome(id);
+        }
+    }
+}
