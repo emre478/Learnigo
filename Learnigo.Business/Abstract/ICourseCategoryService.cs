1e31cbf0303e997449f264a1765a9ec5b423c009
Emre Özer <eozer1371@gmail.com>
2025-05-14 00:04:31 +0300

    Course Category Service has been completed.

---
+++ Learnigo.Business/Abstract/ICourseCategoryService.cs
@@ -0,0 +1,16 @@
+﻿using Learnigo.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learnigo.Business.Abstract
+{
+    public interface ICourseCategoryService : IGenericService<CourseCategory>
+    {
+        void TShowOnHome(int id);
+        void TDontShowOnHome(int id);
+
+    }
+}
