583e169754f1eea078ad0fd139e1d2479b3204a7
Emre Özer <eozer1371@gmail.com>
2025-05-19 21:01:31 +0300

    Components has been completed.

---
+++ Learnigo.WebUI/ViewComponents/Home/_HomeBannerComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learnigo.WebUI.ViewComponents.Home
+{
+    public class _HomeBannerComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
