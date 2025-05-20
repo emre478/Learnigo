982e1a1eb9bdcaa23e33793a92755246421bbb98
Emre Özer <eozer1371@gmail.com>
2025-05-20 22:11:46 +0300

    Course Controller has been edited.

--- Learnigo.API/Controllers/CourseController.cs
+++ Learnigo.API/Controllers/CourseController.cs
@@ -63,5 +63,13 @@ namespace Learnigo.API.Controllers
             _courseService.TDontShowOnHome(id);
             return Ok("Ana Sayfada Gösterilmiyor");
         }
+
+        [HttpGet("GetActiveCourses")]
+
+        public IActionResult GetActiveCourses()
+        {
+            var values = _courseService.TGetFilteredList(x=> x.IsShown==true);
+            return Ok(values);
+        }
     }
 }
