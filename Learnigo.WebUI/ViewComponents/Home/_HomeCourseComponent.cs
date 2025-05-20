5a2c2dbe2406dff75159e912095b0d087df442f6
Emre Özer <eozer1371@gmail.com>
2025-05-20 21:13:25 +0300

    Project has been edited.

---
+++ Learnigo.WebUI/ViewComponents/Home/_HomeCourseComponent.cs
@@ -0,0 +1,16 @@
+﻿using Learnigo.WebUI.Dtos.CourseDtos;
+using Learnigo.WebUI.Helpers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learnigo.WebUI.ViewComponents.Home
+{
+    public class _HomeCourseComponent : ViewComponent
+    {
+        private readonly HttpClient _client = HttpClientInstance.CreateClient();
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var values = await _client.GetFromJsonAsync<List<ResultCourseDto>>("Course/GetActiveCourses");
+            return View(values);
+        }
+    }
+}
