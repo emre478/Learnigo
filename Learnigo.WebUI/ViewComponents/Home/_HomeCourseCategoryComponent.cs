7b5ca05b0105a5f92f679bc1be45a30e9bf439aa
Emre Özer <eozer1371@gmail.com>
2025-05-20 18:31:22 +0300

    Course Category UI has been completed.

--- Learnigo.WebUI/ViewComponents/Home/_HomeCourseCategoryComponent.cs
+++ Learnigo.WebUI/ViewComponents/Home/_HomeCourseCategoryComponent.cs
@@ -9,7 +9,7 @@ namespace Learnigo.WebUI.ViewComponents.Home
         private readonly HttpClient _client = HttpClientInstance.CreateClient();
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var values = await _client.GetFromJsonAsync<List<ResultCourseCategoryDto>>("CourseCategories");
+            var values = await _client.GetFromJsonAsync<List<ResultCourseCategoryDto>>("CourseCategories/GetActiveCategories");
             return View(values);
         }
     }
